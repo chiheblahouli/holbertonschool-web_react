e6a02df5800b3919dd395eda275f316316c290cf
chiheblahouli <chiheb.lahouli@gmail.com>
2022-05-25 13:58:33 +0100

    c

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,67 +1,56 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-		clean: true,
-		publicPath: '/',
-	},
-	mode: 'development',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist/'),
+  },
 	devServer: {
-		contentBase: path.resolve('public'),
+		static: path.resolve('./dist/'),
 		compress: true,
-		port: 8564,
-		hot: true,
-		writeToDisk: true,
-		historyApiFallback: true,
-
-		publicPath: '/',
-		
 	},
-	devtool: 'inline-source-map',
-	performance: {
-		maxAssetSize: 100000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/, use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(gif|png|jpe?g|svg)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true, // webpack@1.x
-							disable: true, // webpack@2.x and newer
-						},
-					},
-				],
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/react']
-					}
-				}
-			},
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/react']
-					}
-				}
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|jpg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        },
+      },
+    ],
+  },
 }
