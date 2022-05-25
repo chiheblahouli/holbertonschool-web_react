1f324aa25aa61d002792fb06c21d81f0f54facd3
chiheblahouli <chiheb.lahouli@gmail.com>
2022-05-25 13:54:13 +0100

    c

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,36 @@
-import logo from './logo.jpg';
+import React from 'react';
+import holbertonLogo from './holberton.jpg'
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+import { Notifications } from './Notifications';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+          <Notifications />
+      <div className="App-header">
+        <img src={holbertonLogo} alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-      
-      <body className="App-body">
+      </div>
+
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
-      
-      <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </footer>
+        <div className="form">
+          <form style={{ "display": "flex", "flex-direction": "column", "width": "20%" }}>
+            <label htmlFor="email">Email</label>
+            <input type="email" style={{ "flex-basis": "0.2" }} name="email" id="email" />
+            <label htmlFor="pwdInput">Password</label>
+            <input type="password" name="pwdInput" id="pwdInput" />
+            <button type="submit">OK</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </div>
+
     </div>
   );
 }
