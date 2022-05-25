cb107d3e035664cde81c6088c9c9cdaad53b5867
chiheblahouli <chiheb.lahouli@gmail.com>
2022-05-25 13:49:10 +0100

    c

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,4 +1,8 @@
-const $ = require( "jquery" );
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+import $ from "jquery";
+
+
+$.when( $.ready ).then(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+  });
